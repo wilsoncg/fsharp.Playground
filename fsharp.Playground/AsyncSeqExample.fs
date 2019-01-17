77496cfe01230ba7e459e4ae7642c5f71b8d9044
Craig Wilson <craig@wilsoncg.net>
2019-01-17 21:22:53 +0000

    async example

--- fsharp.Playground/AsyncSeqExample.fs
+++ fsharp.Playground/AsyncSeqExample.fs
@@ -1,10 +1,41 @@
 ﻿module AsyncSeqExample
 
+open System
+open WebGateway
 open FSharp.Control
 
-// possible example using AsyncSeq<'a>
 // https://fsprojects.github.io/FSharp.Control.AsyncSeq/library/AsyncSeq.html
-let run' = asyncSeq {
-    yield 1
+
+type WebPage = {
+    url : string
+    content : string Option
 }
-run'
+
+let urls = [
+    { url="https://www.google.co.uk"; content=None }
+    { url="https://news.bbc.co.uk"; content=None }
+    { url="https://www.guardian.co.uk"; content=None }
+    { url="https://www.theregister.co.uk"; content=None }]
+
+let getContent (page:WebPage) = 
+    //fetch (Some (Uri page.url))
+    page.url |> Uri |> Some |> fetch
+
+let urls' = asyncSeq { 
+    for u in urls do 
+     try 
+      let! c = getContent u
+      yield { url = u.url; content = Some c }
+     with _ ->
+      yield { url = u.url; content = None }
+} 
+
+// Elements are evaluate asynchronously which could take some time,
+// so use async to avoid blocking
+let run = 
+    async {
+        for u in urls' do
+        match u.content with
+        | Some c -> printfn "%s" c
+        | None -> printfn "no content for %s" u.url
+    } |> Async.Start
--- fsharp.Playground/Program.fs
+++ fsharp.Playground/Program.fs
@@ -11,17 +11,29 @@ let main argv =
         Environment.Exit(0)
     Console.CancelKeyPress.Add(fun _ -> exit())
 
+    let stopwatch = System.Diagnostics.Stopwatch()
+    let time f =
+        stopwatch.Restart()
+        f
+        stopwatch.Stop()
+        printfn "elapsed %6ims" stopwatch.ElapsedMilliseconds
+
     let action = fun _ ->
         printfn ""
         printfn "(Esc)ape, Ctrl-c or (q)uit."
         printfn "run (s)equence example"
-        Console.ReadKey(true)
+        printfn "run (a)sync sequence example"
+        Console.ReadKey(true) // console blocked while waiting for input
     let readKeys = Seq.initInfinite(fun _ -> action())
     let parse (x:ConsoleKeyInfo) =
         match x.Key with 
         | ConsoleKey.S -> 
             printfn "Run (s)"
-            SeqExample.run
+            time (SeqExample.run)
+            None
+        | ConsoleKey.A -> 
+            printfn "Run (a)"
+            time (AsyncSeqExample.run)
             None
         | ConsoleKey.Escape -> Some x.Key
         | ConsoleKey.Q -> Some x.Key
